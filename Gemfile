776d5fe3dae5aadc16b4883ebd7951e3d2c26993
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-03 20:54:12 +0900

    Fix:literalsStyle

--- Gemfile
+++ Gemfile
@@ -59,4 +59,4 @@ end
 gem "tailwindcss-rails"
 gem "tailwindcss-ruby", "3.4.17"
 
-gem 'dartsass-rails'
+gem "dartsass-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root 'static_pages#top'
+  root "static_pages#top"
 end
