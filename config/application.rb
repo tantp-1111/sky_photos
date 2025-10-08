cc5e34c458cb5b36000d5903dffe6a9b0a800b67
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-09 05:59:08 +0900

    fix:lint

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
+    config.time_zone = "Asia/Tokyo"
   end
 end
