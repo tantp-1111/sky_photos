5194252c806d24b0a1dbd8afdd7edffd232d5c3b
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-08 15:39:52 +0900

    add:postMVC’

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
+
+  resources :posts, only: %i[index]
 end
