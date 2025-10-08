c691c0bf0a24dfc8db95cdc6c73f2f0c03f8b0cf
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-08 11:05:08 +0900

    lint,test修正

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    post login_url, params: { email: @user.email, password: 'password' }
+  end
+
   test "should get index" do
     get posts_index_url
     assert_response :success
