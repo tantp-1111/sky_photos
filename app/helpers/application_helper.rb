060878b48c20b7eb373dc5650ea267ce1207e56d
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-08 10:48:17 +0900

    add:フラッシュメッセージ,fix:テスト,tailwind適用修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_message_class(message_type)
+    case message_type
+    when 'success'
+      'text-green-800 bg-green-50 border border-green-300'
+    when 'danger', 'alert', 'error'
+      'text-red-800 bg-red-50 border border-red-300'
+    when 'warning'
+      'text-yellow-800 bg-yellow-50 border border-yellow-300'
+    when 'notice', 'info'
+      'text-blue-800 bg-blue-50 border border-blue-300'
+    else
+      'text-gray-800 bg-gray-50 border border-gray-300'
+    end
+  end
 end
