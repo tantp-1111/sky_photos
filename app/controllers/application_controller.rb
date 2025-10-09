789a3b4a53be12f4e08cc4040d529a8e718e6c00
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-09 12:12:56 +0900

    fix:lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   private
 
   def not_authenticated
-    redirect_to login_path, danger: t('defaults.flash_message.require_login')
+    redirect_to login_path, danger: t("defaults.flash_message.require_login")
   end
 end
