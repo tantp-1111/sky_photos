b089c845119df8968c98d3cdaf0f6cf08bb69bfb
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-14 08:58:00 +0900

    fix:lint

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,13 +2,13 @@ class BookmarksController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
-    redirect_to post_path(params[:post_id]), success: t('.success')
+    redirect_to post_path(params[:post_id]), success: t(".success")
   end
 
   def destroy
     bookmark = current_user.bookmarks.find(params[:id])
     post = bookmark.post
     current_user.unbookmark(post)
-    redirect_to post_path(post), success: t('.success'), status: :see_other
+    redirect_to post_path(post), success: t(".success"), status: :see_other
   end
 end
