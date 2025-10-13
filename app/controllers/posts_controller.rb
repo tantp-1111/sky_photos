09c0d946f0951b453ad979ae355ee82d370e2cc8
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-13 16:35:38 +0900

    add:postのupdate,destroy実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,22 @@ class PostsController < ApplicationController
     @post = current_user.posts.find(params[:id])
   end
 
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: t('defaults.flash_message.updated', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, success: t('defaults.flash_message.deleted', item: Post.model_name.human), status: :see_other
+  end
+
   private
 
   def post_params
