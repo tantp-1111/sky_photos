16c3e4e2e589450bba24227723bae84ae84f7c41
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-15 09:55:19 +0900

    fix:lint

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,13 +2,11 @@ class BookmarksController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
-
   end
 
   def destroy
     bookmark = current_user.bookmarks.find(params[:id])
     @post = bookmark.post
     current_user.unbookmark(@post)
-
   end
 end
