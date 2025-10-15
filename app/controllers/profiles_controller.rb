0a8b4b610237fa7c70440044bdb560b44b92f9d2
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-15 14:38:07 +0900

    add:マイページ実装

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,25 @@
+class ProfilesController < ApplicationController
+  def show
+    @posts = current_user.posts.order(created_at: :desc)
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('defaults.flash_message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
