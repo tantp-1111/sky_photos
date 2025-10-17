0eec9b036aa670f33186cec4f32f0ec8e1ae8ad4
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-17 15:06:02 +0900

    fix:lint,test3

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,12 +3,13 @@ require "test_helper"
 class UserMailerTest < ActionMailer::TestCase
   test "reset_password_email" do
     user = users(:one)
+    user.generate_reset_password_token!
 
     mail = UserMailer.reset_password_email(user)
 
     assert_equal "Reset password email", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal [ user.email ], mail.to
+    assert_equal [ "from@example.com" ], mail.from
     assert_match user.email, mail.body.encoded
   end
 end
