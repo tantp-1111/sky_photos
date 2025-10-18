d08964ded9ecb588660105b08f96c6dcbccc8bf5
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-18 12:31:15 +0900

    add:本番環境パスワードリセット設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
-    domain:               "skyphotos.onrender.com",
+    domain:               "sky-photos.onrender.com",
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       "plain",
