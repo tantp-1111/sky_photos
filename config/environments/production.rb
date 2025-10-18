5258432592fa437cf1a9828d0b0035c5d3e79093
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-19 00:38:45 +0900

    add:パスワードリセット修正production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
