211c835431e957d5b49cbe7f89bf27da2bb23eba
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-21 10:59:14 +0900

    fix:プレビュ表示コントローラー修正

--- app/javascript/controllers/previews_controller.js
+++ app/javascript/controllers/previews_controller.js
@@ -8,26 +8,29 @@ export default class extends Controller {
     console.log("Preview controller connected")
   }
 
+  currentObjectURL = null
+
   preview(event) {
-    const file = event.target.files[0]   //選択されたファイルを取得
+    const file = event.target.files[0]
+    //選択されたファイルを取得
 
     if (file) {
-      if (file.type.startsWith('image/')) {   // 画像ファイルかどうかを確認
-        const reader = new FileReader()
+      // MIMEタイプで画像ファイルかどうかを確認
+      if (file.type.startsWith('image/')) {
+        // 既存のオブジェクトURLがあれば解放
+        this.revokeCurrentObjectURL()
 
-        reader.onload = (e) => {
-          this.imageTarget.src = e.target.result   // 画像のsrcに設定
-          this.previewTarget.classList.remove('hidden')   // プレビューを表示
-        }
+        // 新しいオブジェクトURLを作成
+        this.currentObjectURL = URL.createObjectURL(file)
 
-        reader.readAsDataURL(file)   // ファイルをData URLとして読み込み
+        this.imageTarget.src = this.currentObjectURL // srcに設定
+        this.previewTarget.classList.remove('hidden')  // プレビューを表示
       } else {
         alert('画像ファイルを選択してください。')
-        this.inputTarget.value = ''   // 入力フィールドをクリア
+        this.inputTarget.value = ''
       }
     } else {
-      this.hidePreview()   // ファイルが選択されていない場合は非表示
-
+      this.hidePreview()
     }
   }
 
@@ -39,5 +42,19 @@ export default class extends Controller {
   hidePreview() {
     this.previewTarget.classList.add('hidden')  // CSSクラスで非表示
     this.imageTarget.src = ''   // 画像のsrcをクリア
+    this.revokeCurrentObjectURL()  // オブジェクトURLを解放
+  }
+
+  // オブジェクトURLの解放処理
+  revokeCurrentObjectURL() {
+    if (this.currentObjectURL) {
+      URL.revokeObjectURL(this.currentObjectURL)
+      this.currentObjectURL = null
+    }
+  }
+
+  // コントローラーが破棄される時にもクリーンアップ
+  disconnect() {
+    this.revokeCurrentObjectURL()
   }
 }
