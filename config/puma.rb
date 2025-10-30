5000c4ee437690832e5048d89fc57332b45f36f9
瀧澤匠子 <sthkm1111@gmail.com>
2025-10-30 21:06:25 +0900

    fix:メモリ使用効率改善

--- config/puma.rb
+++ config/puma.rb
@@ -20,7 +20,8 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# メモリ使用量最適化のため3から2に変更
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 2)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
